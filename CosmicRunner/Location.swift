cb266b8c706d973aab51337a59aafecc678fd9aa
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-07-26 23:50:22 +0600

    added core data models

---
+++ CosmicRunner/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  CosmicRunner
+//
+//  Created by ANDROMEDA on 7/26/16.
+//  Copyright © 2016 infancyit. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Location: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ CosmicRunner/Run.swift
@@ -0,0 +1,17 @@
+//
+//  Run.swift
+//  CosmicRunner
+//
+//  Created by ANDROMEDA on 7/26/16.
+//  Copyright © 2016 infancyit. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Run: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
